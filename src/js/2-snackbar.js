6c5cf0592e5c8b8559f42e8b3e61918dec8213cf
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-07-13 22:54:44 +0200

    summary

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,39 @@
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = parseInt(e.target.elements.delay.value.trim(), 10);
+  const state = e.target.elements.state.value;
+
+  const makePromise = ({ delay, shouldResolve }) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve();
+        } else {
+          reject();
+        }
+      }, delay);
+    });
+  };
+
+  const shouldResolve = state === 'fulfilled';
+
+  makePromise({ delay, shouldResolve })
+    .then(() => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(() => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
